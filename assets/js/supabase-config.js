495cec90c3d38d27228c46855638e38b2f7ea92a
marcelovianna-salux <marcelo.vianna@salux.com.br>
2026-05-12 08:35:49 -0300

    Update supabase-config.js

--- assets/js/supabase-config.js
+++ assets/js/supabase-config.js
@@ -1,4 +1,8 @@
 const SUPABASE_URL = 'https://kbuqchahmlasfwysrodv.supabase.co';
 const SUPABASE_ANON_KEY = 'sb_publishable_nh_eqNALJN2IslmFI6wvaA_ryaMTKVm';
 const { createClient } = supabase;
-const _supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+const _supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
+  auth: {
+    persistSession: false
+  }
+});
